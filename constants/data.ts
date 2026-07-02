265207efe3b6a784bac23d9b04847081ce5c6877
Alisa-trigger <firstyearclassics11@gmail.com>
2026-07-02 17:18:17 +0500

    feat: project setup , nav,routing , tab navigation

---
+++ constants/data.ts
@@ -0,0 +1,11 @@
+import { icons } from "./icon";
+
+export const tabs = [
+    { name: "index", title: "Home", icon: icons.home },
+
+    { name: "insights", title: "Insights", icon: icons.activity },
+
+    { name: "subscriptions", title: "Subscriptions", icon: icons.wallet },
+
+    { name: "settings", title: "Settings", icon: icons.setting },
+]
